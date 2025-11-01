2e608b7accea31cf08c036351a77d5c23484856c
anonym <anonym@192.168.1.11>
2025-11-01 20:01:51 +0300

    start structure basement

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import Criteria from './pages/Criteria.tsx'
+import Criterion from './pages/Criterion.tsx'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/criteria" element={<Criteria />} />
+        <Route path="/criteria/:id" element={<Criterion />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
---
+++ src/pages/Criterion.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Criterion = () => {
+  return (
+    <div>Criterion</div>
+  )
+}
+
+export default Criterion
